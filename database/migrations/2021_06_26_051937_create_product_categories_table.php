800113f7573743ce459282d98e5047c47655aa54
Samir Shrestha <theonlysamir@gmail.com>
2021-06-26 11:11:29 +0545

    Product Category added with seeds

--- database/migrations/2021_06_26_051937_create_product_categories_table.php
+++ database/migrations/2021_06_26_051937_create_product_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePropertyCategoriesTable extends Migration
+class CreateProductCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePropertyCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('property_categories', function (Blueprint $table) {
+        Schema::create('product_categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('image')->nullable();
@@ -28,6 +28,6 @@ class CreatePropertyCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('property_categories');
+        Schema::dropIfExists('product_categories');
     }
 }
--- database/seeders/ProductCategorySeeder.php
+++ database/seeders/ProductCategorySeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class PropertyCategorySeeder extends Seeder
+class ProductCategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -14,23 +14,23 @@ class PropertyCategorySeeder extends Seeder
      */
     public function run()
     {
-        DB::table('property_categories')->insert([
+        DB::table('product_categories')->insert([
             'name' => 'Fruits',
             'image'=> '/images/tests/categories/fruits.jpg'
         ]);
-        DB::table('property_categories')->insert([
+        DB::table('product_categories')->insert([
             'name' => 'Liquor',
             'image'=> '/images/tests/categories/liquor.jpg'
         ]);
-        DB::table('property_categories')->insert([
+        DB::table('product_categories')->insert([
             'name' => 'Electronics',
             'image'=> '/images/tests/categories/electronics.jpg'
         ]);
-        DB::table('property_categories')->insert([
+        DB::table('product_categories')->insert([
             'name' => 'Essentials',
             'image'=> '/images/tests/categories/essentials.jpg'
         ]);
-        DB::table('property_categories')->insert([
+        DB::table('product_categories')->insert([
             'name' => 'Luxury',
             'image'=> '/images/tests/categories/luxury.jpg'
         ]);
